ace554afb836b82b5a3489dc2e5c17b45470aadc
Pierre DAVID <pda@unistra.fr>
2013-04-09 08:28:38 +0200

    First attempt to get directory structure.

---
+++ include/defs.h
@@ -0,0 +1,10 @@
+/*
+ * SOS common definitions
+ *
+ */
+
+// http://stackoverflow.com/questions/3366812/linux-raw-ethernet-socket-bind-to-specific-protocol
+#define	ETHTYPE_SOS	0x88b5		// public use for prototype
+
+// Ethernet address length
+#define	ETHADDRLEN	6
