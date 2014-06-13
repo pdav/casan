852233f8c83fe9d2da08b86ad4a297cdbae44d69
Robin Chavignat <robin.chavignat@etu.unistra.fr>
2014-06-13 13:30:04 +0200

    Added val function to enum class Fixed : reverse dictionary not working when enum is initialized from a dict

--- pySos/enum.py
+++ pySos/enum.py
@@ -1,3 +1,5 @@
+from types import MethodType
+
 '''
 This class implements enumeration behavior
 It is used to store a collection of constant value pairs (as long as you 
@@ -6,13 +8,21 @@ don't mess with it by hand, but why would you ?)
 class Enum:
     def __new__(cls, enumName, d):
         if type(d).__name__ == 'dict':
-            e = type(enumName, (object,), d)
             reverse = dict(zip(d.values(), d.keys()))
             d['reverse'] = reverse
+            e = type(enumName, (object,), d)
+            e.val = MethodType(cls.val, e)
             return e
         else:
             dic = dict(zip(d, range(len(d))))
             dicReverse = dict(zip(range(len(d)), d))
             dic['reverse'] = dicReverse
             e = type(enumName, (object,), dic)
+            e.val = MethodType(cls.val, e)
             return e
+
+    def val(self, key):
+        try:
+            return self.__dict__[key]
+        except KeyError:
+            raise
