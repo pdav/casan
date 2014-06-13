30583ded5707444bc652b45dbb5d7a5cb5a5a8c5
Robin Chavignat <robin.chavignat@etu.unistra.fr>
2014-06-13 17:00:42 +0200

    Added l2.py : contains abstract base classes for L2 addresses and networks

---
+++ pySos/l2.py
@@ -0,0 +1,77 @@
+from collections import abc
+
+'''
+This module defines the various classes used for L2 network addressing.
+'''
+
+class l2addr(metaclass = abc.ABCMeta):
+    '''
+    This abstract class is the base for all l2addr-* subclasses
+    '''
+    @abc.abstractmethod
+    def __eq__(self, other):
+        '''
+        Equality test operator
+        '''
+        pass
+
+    @abc.abstractmethod
+    def __ne__(_self, other):
+        '''
+        Difference test operator
+        '''
+        pass
+        
+    @abc.abstractmethod
+    def __repr__(self):
+        '''
+        Compact string representation
+        '''
+        pass
+
+    @abc.abstractmethod
+    def __str__(self):
+        '''
+        "Pretty" string representation
+        '''
+        pass
+
+class l2net(metaclass = abc.ABCMeta):
+    '''
+    This abstract class is the base for all l2net-* subclasses
+    '''
+    def __init__(self):
+        self.mtu = 0
+        self.max_latency = 0
+
+    @abc.abstractmethod
+    def term(self):
+        '''
+        '''
+        pass
+
+    @abc.abstractmethod
+    def send(self, daddr, data):
+        '''
+        '''
+        pass
+
+    @abc.abstractmethod
+    def bsend(self, data):
+        '''
+        '''
+        pass
+
+    @abc.abstractmethod
+    def recv(self, saddr, data):
+        '''
+        '''
+        pass
+
+    @abc.abstractmethod
+    def bcastaddr(self):
+        '''
+        '''
+        pass
+
+
