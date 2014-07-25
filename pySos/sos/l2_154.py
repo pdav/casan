13d1e7805d847041cd864e4ec2d98de7806a9277
Robin Chavignat <robin.chavignat@etu.unistra.fr>
2014-07-25 14:21:11 +0200

    In l2_154.py: Added missing documentation. Changed some names for clarity. Added a couple of comments.

--- pySos/sos/l2_154.py
+++ pySos/sos/l2_154.py
@@ -75,7 +75,7 @@ class l2net_154(l2.l2net):
     broadcast = l2addr_154('ff:ff:ff:ff:ff:ff:ff:ff')
 
     # Enums
-    class frame_type(IntEnum):
+    class FrameType(IntEnum):
         """
         Enumerates frame types.
         TODO : use a regular Enum (gotta change every occurence of it though)
@@ -91,9 +91,10 @@ class l2net_154(l2.l2net):
         """
         s = 'Network type : 802.15.4\n'
         s = s + 'Interface : ' + self.iface + '\n'
-        s = s + 'Interface address : ' + str(self.a) + '\n'
-        s = s + 'PANID : ' + str(self.pan) + '\n'
+        s = s + 'Interface address : ' + str(self.addr) + '\n'
+        s = s + 'PANID : ' + str(self.panid) + '\n'
         s = s + 'MTU : ' + str(self.mtu) + '\n'
+        return s
 
     def __init__(self):
         """
@@ -104,7 +105,11 @@ class l2net_154(l2.l2net):
         self.fd = -1
         self.framelist = []
         self.buffer = bytearray()
-        self.iface = None
+        self.iface = ''
+        self.addr = None
+        self.panid = None
+        self.channel = 0
+        self.port = None
 
     def init(self, iface, type_, mtu, addr, panid, channel, timeout=None):
         """
@@ -125,8 +130,8 @@ class l2net_154(l2.l2net):
                         and will have to be killed.
         :return: True if the operation succeeds, False otherwise.
         """
-        self.a = l2addr_154(addr)
-        self.pan = l2addr_154(panid)
+        self.addr = l2addr_154(addr)
+        self.panid = l2addr_154(panid)
         self.channel = channel
 
         if type_ == 'xbee':
@@ -144,23 +149,24 @@ class l2net_154(l2.l2net):
                              0,
                              termios.CS8 | termios.CREAD | termios.B9600,
                              0]
-                for attr in attrs[6]:
+                #for attr in attrs[6]:
                     # POSIX_VDISABLE may not be implemented in python, at 
                     # least I couldn't find any information about it, maybe
                     # check in python stdlib source code?
                     #attr = termios.POSIX_VDISABLE
-                    pass
                 termios.tcsetattr(self.fd, termios.TCSANOW, attrs)
 
                 def send_AT_command(s):
                     """
                     This function sends an AT command over the serial port and checks for the returned value.
-                    It may seem a bit 'too much', but it is the best solution I could come up with.
+                    It may seem a bit 'too much', but it is the best solution I came up with.
+                    :param s: AT command to send.
+                    :raise: RuntimeError: an error occured.
                     """
                     self.port.write(s)
                     r = bytearray()
                     while True:
-                        c = self.port.read()
+                        c = self.port.read()  # Read a byte at a time
                         r += c
                         if c == b'\r':
                             if r[-3:] == b'OK\r':
@@ -177,13 +183,13 @@ class l2net_154(l2.l2net):
                     send_AT_command(b'ATRE\r')
 
                     # Short address
-                    sa = (b'ATMY' + bytes(hex(self.a.addr[1])[2:], 'utf-8') +
-                          bytes(hex(self.a.addr[0])[2:], 'utf-8') + b'\r')
+                    sa = (b'ATMY' + bytes(hex(self.addr.addr[1])[2:], 'utf-8') +
+                          bytes(hex(self.addr.addr[0])[2:], 'utf-8') + b'\r')
                     send_AT_command(sa)
 
                     # PANID
-                    panid = (b'ATID' + bytes(hex(self.pan.addr[1])[2:], 'utf-8') +
-                             bytes(hex(self.pan.addr[0])[2:], 'utf-8') + b'\r')
+                    panid = (b'ATID' + bytes(hex(self.panid.addr[1])[2:], 'utf-8') +
+                             bytes(hex(self.panid.addr[0])[2:], 'utf-8') + b'\r')
                     send_AT_command(panid)
 
                     # Channel
@@ -200,7 +206,7 @@ class l2net_154(l2.l2net):
                     send_AT_command(b'ATCN\r')
 
                     self.port.flushInput()
-                except RuntimeError as e: # AT command error
+                except RuntimeError as e:  # AT command error
                     print(e)
                     return False
             except Exception as e:
@@ -218,20 +224,21 @@ class l2net_154(l2.l2net):
         self.port.close()
         self.fd = -1
 
-    def encode_transmit(self, destAddr, data):
+    def encode_transmit(self, dest_addr, data):
         """
         Creates a 802.15.4 frame
+        :param dest_addr: destination address as an instance of l2addr_154.
+        :param data: data to put in the frame.
         """
         fdlen = 5 + len(data)
-        cmdlen = 4 + fdlen
         b = bytearray()
         b.append(self.XBEE_START)
         b.append((fdlen & 0xFF00) >> 8)
         b.append(fdlen & 0x00FF)
         b.append(self.XBEE_TX_SHORT)
         b.append(0x41)
-        b.append(destAddr.addr[1])
-        b.append(destAddr.addr[0])
+        b.append(dest_addr.addr[1])
+        b.append(dest_addr.addr[0])
         b.append(0)
         b += data
         b.append(self.compute_checksum(b))
@@ -241,12 +248,18 @@ class l2net_154(l2.l2net):
     @staticmethod
     def compute_checksum(buf):
         """
-        Computes the checksum of a ZigBee frame.
+        Computes the checksum of a ZigBee frame
+        :param buf: a complete frame from the frame list.
+        :return: an integer between 0 and 255 representing the checksum of the frame.
         """
         def i16_from_2i8(i1, i2):
             """
             Returns a 16bits integer made from 2 8bits integers.
-            The first integer will be the most significant byte of the result
+            No checks are performed on the integers, so make sure their value
+            is between 0 and 255 included.
+            :param i1: most signifigant byte
+            :param i2: least significant byte
+            :return: concatenation of i1 and i2
             """
             return (i1 << 8) | i2
         paylen = i16_from_2i8(buf[1], buf[2]) + 4
@@ -258,18 +271,20 @@ class l2net_154(l2.l2net):
     def send(self, dest_slave, data):
         """
         Sends a frame over the network.
-        :return: True if success, False either.
+        :param dest_slave: recipient slave.
+        :param data: data to send.
+        :return: True if success, else False.
         """
         if len(data) <= self.L2_154_MTU:
             cmd = self.encode_transmit(dest_slave.addr, data)
-            cmdLen = len(cmd)
+            cmd_len = len(cmd)
             try:
-                while cmdLen > 0:
-                    r = self.port.write(cmd[:cmdLen])
+                while cmd_len > 0:
+                    r = self.port.write(cmd[:cmd_len])
                     if r == 0:
                         return False
                     else:
-                        cmdLen = cmdLen - r
+                        cmd_len = cmd_len - r
                         cmd = cmd[r:]
                 return True
             except Exception:
@@ -279,6 +294,7 @@ class l2net_154(l2.l2net):
     def bsend(self, data):
         """
         Broadcasts a frame over the network.
+        :param data: data to broadcast.
         """
         return self.send(self.broadcast, data)
 
@@ -314,7 +330,7 @@ class l2net_154(l2.l2net):
         r = l2.PkTypes.PK_NONE
         a, data, = None, None
         for frame in self.framelist:
-            if frame.type == self.frame_type.RX_SHORT:
+            if frame.type == self.FrameType.RX_SHORT:
                 a = l2addr_154(hex(frame.rx_short.saddr & 0x00FF)[2:] + ':' +
                                hex((frame.rx_short.saddr & 0xFF00) >> 8)[2:])
                 data = frame.rx_short.data
@@ -364,7 +380,7 @@ class l2net_154(l2.l2net):
             if self.XBEE_START not in self.buffer:
                 # No start byte found
                 break
-            # Align start byte to buffer start
+            # Align buffer start to start byte
             self.buffer = self.buffer[self.buffer.index(self.XBEE_START):]
 
             # Buffer long enough to extract frame size?
@@ -374,7 +390,7 @@ class l2net_154(l2.l2net):
 
             framelen = (self.buffer[1] << 8) | self.buffer[2]
             if framelen > self.XBEE_MAX_FRAME_SIZE:
-                buffer = self.buffer[1:]
+                self.buffer = self.buffer[1:]
                 continue
             
             # Got checksum?
@@ -384,12 +400,11 @@ class l2net_154(l2.l2net):
 
             cksum = self.compute_checksum(self.buffer)
             if cksum != self.buffer[pktlen - 1]:
-                buffer = self.buffer[1:]
+                self.buffer = self.buffer[1:]
                 continue
             invalid, complete = False, True
         # At this point, we have a full valid frame in the buffer
         return complete
-
 
     def extract_frame_to_list(self):
         """
@@ -397,20 +412,27 @@ class l2net_154(l2.l2net):
         received frames list.
         """
         class Frame:
-            pass
+            """
+            Represents a 802.15.4 frame
+            :return:
+            """
+            def __init__(self):
+                self.tx_status = None
+                self.rx_short = None
+                self.type = None
         framelen = (self.buffer[1] << 8) | self.buffer[2]
         pktlen = framelen + 4
         f = Frame()
         f.type = self.buffer[3]
-        if f.type == self.frame_type.TX_STATUS:
+        if f.type == self.FrameType.TX_STATUS:
             f.tx_status = Frame()
             f.tx_status.frame_id = self.buffer[4]
             f.tx_status.status = self.buffer[5]
-        elif f.type == self.frame_type.RX_SHORT:
+        elif f.type == self.FrameType.RX_SHORT:
             f.rx_short = Frame()
             f.rx_short.saddr = (self.buffer[4] << 8) | self.buffer[5]
             f.rx_short.len = framelen - 5
-            f.rx_short.data = bytes(self.buffer[8 : f.rx_short.len + 8])
+            f.rx_short.data = bytes(self.buffer[8: f.rx_short.len + 8])
             f.rx_short.rssi = self.buffer[6]
             f.rx_short.options = self.buffer[7]
         else:
@@ -419,4 +441,4 @@ class l2net_154(l2.l2net):
             return
         self.framelist.append(f)
         self.buffer = (bytearray() if len(self.buffer) == pktlen 
-                                   else self.buffer[pktlen:])
+                       else self.buffer[pktlen:])
