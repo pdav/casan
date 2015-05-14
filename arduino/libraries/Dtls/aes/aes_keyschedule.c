be89b63954a3ae25c15ef81f478189ab42e8ca62
karchnu <karchnu@karchnu.fr>
2015-05-15 01:29:55 +0200

    suppression de toutes les parties non utiles

--- arduino/libraries/Dtls/aes/aes_keyschedule.c
+++ arduino/libraries/Dtls/aes/aes_keyschedule.c
@@ -1,30 +1,3 @@
-/* aes_keyschedule.c */
-/*
- This file is part of the AVR-Crypto-Lib.
- Copyright (C) 2006-2015 Daniel Otte (bg@nerilex.org)
-
- This program is free software: you can redistribute it and/or modify
- it under the terms of the GNU General Public License as published by
- the Free Software Foundation, either version 3 of the License, or
- (at your option) any later version.
-
- This program is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- GNU General Public License for more details.
-
- You should have received a copy of the GNU General Public License
- along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
-/**
- * \file     aes_keyschedule.c
- * \email    bg@nerilex.org
- * \author   Daniel Otte
- * \date     2008-12-30
- * \license  GPLv3 or later
- *
- */
-
 #include <stdint.h>
 #include "aes.h"
 #include "aes_keyschedule.h"
@@ -32,6 +5,7 @@
 #include <string.h>
 #include <avr/pgmspace.h>
 
+#if 0
 static
 void aes_rotword(void *a)
 {
@@ -42,6 +16,7 @@ void aes_rotword(void *a)
     ((uint8_t*) a)[2] = ((uint8_t*) a)[3];
     ((uint8_t*) a)[3] = t;
 }
+#endif
 
 #if 0
 const uint8_t rc_tab[] PROGMEM = { 0x01, 0x02, 0x04, 0x08,
@@ -95,15 +70,3 @@ void aes128_init(const void *key, aes128_ctx_t *ctx)
     aes_init(key, 128, (aes_genctx_t*) ctx);
 #endif
 }
-
-#if 0
-void aes192_init(const void *key, aes192_ctx_t *ctx)
-{
-    aes_init(key, 192, (aes_genctx_t*) ctx);
-}
-
-void aes256_init(const void *key, aes256_ctx_t *ctx)
-{
-    aes_init(key, 256, (aes_genctx_t*) ctx);
-}
-#endif
--- arduino/libraries/Dtls/aes/aes_keyschedule.h
+++ arduino/libraries/Dtls/aes/aes_keyschedule.h
@@ -1,30 +1,3 @@
-/* aes_keyschedule.h */
-/*
- This file is part of the AVR-Crypto-Lib.
- Copyright (C) 2006-2015 Daniel Otte (bg@nerilex.org)
-
- This program is free software: you can redistribute it and/or modify
- it under the terms of the GNU General Public License as published by
- the Free Software Foundation, either version 3 of the License, or
- (at your option) any later version.
-
- This program is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- GNU General Public License for more details.
-
- You should have received a copy of the GNU General Public License
- along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
-/**
- * \file     aes_keyschedule.h
- * \email    bg@nerilex.org
- * \author   Daniel Otte
- * \date     2008-12-30
- * \license  GPLv3 or later
- * \ingroup  AES
- */
-
 #ifndef AES_KEYSCHEDULE_H_
 #define AES_KEYSCHEDULE_H_
 
@@ -51,27 +24,5 @@ void aes_init(const void *key, uint16_t keysize_b, aes_genctx_t *ctx);
  * \param ctx       pointer to the context where the keyschedule should be stored
  */
 void aes128_init(const void *key, aes128_ctx_t *ctx);
-
-#if 0
-/**
- * \brief initialize the keyschedule for 192 bit key
- *
- * This function computes the keyschedule from a given 192 bit key
- * and stores it in the context variable
- * \param key       pointer to the key material
- * \param ctx       pointer to the context where the keyschedule should be stored
- */
-void aes192_init(const void *key, aes192_ctx_t *ctx);
-
-/**
- * \brief initialize the keyschedule for 256 bit key
- *
- * This function computes the keyschedule from a given 256 bit key
- * and stores it in the context variable
- * \param key       pointer to the key material
- * \param ctx       pointer to the context where the keyschedule should be stored
- */
-void aes256_init(const void *key, aes256_ctx_t *ctx);
-#endif
 
 #endif /* AES_KEYSCHEDULE_H_ */
