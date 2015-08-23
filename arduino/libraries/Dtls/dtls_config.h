65e872a1b35907f58e086c8513ee98e5648112da
karchnu <karchnu@karchnu.fr>
2015-08-23 22:08:54 +0200

    MSG_DISPLAY_ENCRYPT defined

--- arduino/libraries/Dtls/dtls_config.h
+++ arduino/libraries/Dtls/dtls_config.h
@@ -127,7 +127,7 @@
 //#define MSG_DEBUG 1
 
 #define MSG_DURATION 1
-//#define MSG_ENCRYPT_TIME 1
+#define MSG_ENCRYPT_TIME 1
 //#define MESURE_TIME_START_PUSHING_MSG
 
 /* Define WORDS_BIGENDIAN to 1 if your processor stores words with the most
